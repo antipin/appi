245578be0b61d8ec5389cc8ded262012842a3285
Alexander Antipin <alex@antipin.com>
2016-10-13 22:23:58 +0300

    Remove unused config params from localFS component

--- src/components/local-fs/local-fs.config.js
+++ src/components/local-fs/local-fs.config.js
@@ -10,31 +10,18 @@ export function getConfig(env) {
 
     const {
         LOCAL_FS_PATH = '/var/appi-file-storage/',
-        LOCAL_FS_EXTENSIONS = 'jpg,jpeg,gif,png,txt,pdf',
-        /**
-         * Maximum file size in bytes
-         * @type {number}
-         */
-        LOCAL_FS_MAX_SIZE = 50 * 1024 * 1024
     } = env
 
     return {
         data: {
             path: LOCAL_FS_PATH,
-            allowedExtensions: LOCAL_FS_EXTENSIONS,
-            maxSizeInBytes: LOCAL_FS_MAX_SIZE
         },
         schema: {
             path: joi.string()
                 .required(),
-            allowedExtensions: joi.string()
-                .required(),
-            maxSizeInBytes: joi.number()
-                .required(),
         },
         processing: {
             path: value => path.join(value, '/'), // ensure trailing slash
-            allowedExtensions: value => value.split(','),
         }
     }
 
